9fd97c4bca6861638f88bfa4a8be7c289dd4e107
Phillip Oldham <phill@activityhq.com>
2011-01-25 13:59:37 +0000

    generated docs

---
+++ searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({objects:{"":{PySO8601:[0,0,1]}},terms:{pyso8601:0,index:0,search:0,welcom:0,modul:0,indic:0,content:0,tabl:0,document:0,page:0},objtypes:{"0":"py:module"},titles:["Welcome to PySO8601&#8217;s documentation!"],objnames:{"0":"Python module"},filenames:["index"]})
