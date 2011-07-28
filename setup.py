c35f1cea805460ed52a679ac5f3c2d04e3e6cc55
Phillip B Oldham <phillip.oldham@exstatik.com>
2011-07-28 11:13:48 +0100

    fixing issue with setuptools; importing README.md when file is now README.rst

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os, sys
 execfile('PySO8601/__version__.py')
 
 setup(name="PySO8601",
-      description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+      description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
       version=__version__,
       url="http://digitala.github.com/PySO8601",
       packages=find_packages(exclude="specs"),
