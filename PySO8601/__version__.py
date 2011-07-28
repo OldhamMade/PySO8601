f06e6c92152add26a75b40e5cf38b8d0c52f79cb
Phillip B Oldham <phillip.oldham@exstatik.com>
2011-07-28 12:04:02 +0100

    fixing version number for setuptools

--- PySO8601/__version__.py
+++ PySO8601/__version__.py
@@ -1,1 +1,3 @@
-__version__ = [0,1,2]
+__author__	= 'Phillip B Oldham'
+__version__	= '0.1.3'
+__licence__	= 'MIT'
--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ execfile('PySO8601/__version__.py')
 
 setup(name="PySO8601",
       description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
-      version=__version__,
-      url="http://digitala.github.com/PySO8601",
+      version=(0.1.3), #__version__,
+      url="http://unpluggd.github.com/PySO8601",
       packages=find_packages(exclude="specs"),
       )
       
