4655acf2add2343b02abd976d147b3030bc85737
Phillip B Oldham <phillip.oldham@exstatik.com>
2011-07-28 12:24:01 +0100

    fixing version number for setuptools

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 import os, sys
 
-#execfile('PySO8601/__version__.py')
+execfile('PySO8601/__version__.py')
 
 setup(name="PySO8601",
       description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
       url="http://unpluggd.github.com/PySO8601",
-      version="0.1.3",
+      version=__version__,
       packages=find_packages(exclude="specs"),
       )
       
