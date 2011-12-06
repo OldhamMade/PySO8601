bf00164ce42a28cd2134dae3f4d7f5b043e89918
Phillip Oldham <phillip.oldham@gmail.com>
2011-12-06 22:00:33 +0000

    adding author data in preparation for pushing to pypi

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ setup(name="PySO8601",
       description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
       url="http://unpluggd.github.com/PySO8601",
       version=__version__,
+      author=__author__,
+      author_email=__author_email__,
       packages=find_packages(exclude="specs"),
       )
-      
+
