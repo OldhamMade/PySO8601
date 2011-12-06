37c28c3e6cbbd1025798dde420d09b63a839c8f1
Phillip Oldham <phillip.oldham@gmail.com>
2011-12-06 17:36:06 +0000

    made some attrs 'private' so that they don't show up in the docs

--- PySO8601/timezones.py
+++ PySO8601/timezones.py
@@ -6,19 +6,19 @@ class Timezone(datetime.tzinfo):
 
     ``tzstring`` accepts a valid ISO8601 timezone string or ``None`` for UTC.
     """
-    ZERO = datetime.timedelta(0)
-    regex = re.compile(r'^(?P<prefix>\+|-)(?P<hours>\d{2})(:?(?P<mins>\d{2}))?')
+    _ZERO = datetime.timedelta(0)
+    _regex = re.compile(r'^(?P<prefix>\+|-)(?P<hours>\d{2})(:?(?P<mins>\d{2}))?')
 
     def __init__(self, tzstring=None):
-        self.__offset = self.ZERO
+        self.__offset = self._ZERO
         self.__name = 'UTC'
 
         if tzstring is None or tzstring.strip() in ('Z', ''):
             return
-        
-        found = self.regex.search(tzstring.strip()).groupdict()
+
+        found = self._regex.search(tzstring.strip()).groupdict()
         prefix, hours, mins = found['prefix'], int(found['hours']), int(found['mins'] or 0)
-        
+
         self.__name = "UTC%s%02d:%02d" % (prefix, hours, mins)
 
         if prefix == '-':
@@ -31,12 +31,10 @@ class Timezone(datetime.tzinfo):
         return self.__offset
 
     def dst(self, dt):
-        return self.ZERO
+        return self._ZERO
 
     def __repr__(self):
         return "<Timezone %s>" % self.__name
 
     def __str__(self):
         return self.__name
-    
-
