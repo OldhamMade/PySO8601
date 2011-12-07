693c4d776464328264da1bce8a7b0e4990dc8de3
Phillip Oldham <phillip.oldham@gmail.com>
2011-12-07 21:43:01 +0000

    updates for distribution

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ import os, sys
 execfile('PySO8601/__version__.py')
 
 setup(name="PySO8601",
-      description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+      description="PySO8601 aims to parse any ISO 8601 date, including Week dates, Ordinal dates, intervals and durations.",
+      long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
       url="http://unpluggd.github.com/PySO8601",
       version=__version__,
       author=__author__,
