16e0f24ec256923a9a921135313842add7929b3d
Phillip Oldham <phillip.oldham@gmail.com>
2012-01-31 20:03:02 +0000

    fixing issues with setup.py

--- PySO8601/__version__.py
+++ PySO8601/__version__.py
@@ -1,4 +1,4 @@
 __author__	= 'Phillip B Oldham'
 __author_email__ = 'phillip.oldham@gmail.com'
-__version__	= '0.1.3'
+__version__	= '0.1.5'
 __licence__	= 'MIT'
--- setup.py
+++ setup.py
@@ -3,9 +3,15 @@ import os, sys
 
 execfile('PySO8601/__version__.py')
 
+desc_file = os.path.join(os.path.dirname(__file__), 'README.rst')
+long_description = ''
+
+if os.path.isfile(desc_file):
+    long_description = open(desc_file).read()
+
 setup(name="PySO8601",
       description="PySO8601 aims to parse any ISO 8601 date, including Week dates, Ordinal dates, intervals and durations.",
-      long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+      long_description=long_description,
       url="http://unpluggd.github.com/PySO8601",
       version=__version__,
       author=__author__,
